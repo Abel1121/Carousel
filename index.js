d9703aa4057ce118be338ea83bab09e6f07e5489
Abel1121 <Marecki1121@gmail.com>
2019-09-26 23:51:00 +0200

    fix: carousel source

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {Carousel} from "./carousel.js.js";
+import {Carousel} from "carousel.js";
     const arrowRight = document.querySelector(".arrow-right");
     const arrowLeft = document.querySelector(".arrow-left");
 const carousel = new Carousel;
